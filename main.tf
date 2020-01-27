80a1c0923cd595abe04ba2bed3f550af56201b59
Joshua Mathianas <mathianasj@cloudfirst.dev>
2020-01-27 13:45:17 -0500

    remove testing files from repo

--- main.tf
+++
@@ -1,9 +0,0 @@
-provider "kea" {
-    url     = "http://192.168.16.2:8080"
-}
-
-resource "kea_reservation" "my-server" {
-    ip_address = "192.0.5.4"
-    hw_address = "5a:00:2d:48:43:05"
-    subnet_id = 2
-}
